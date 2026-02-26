399f3554da4cb2b4227fa21aa769daf5ef553f4a
Hamdi Emad <hamdyemadelgohary2004@gmail.com>
2026-02-26 17:32:11 +0200

    Task 5

--- MySQL/Task_5/Task_5_Solutions.sql
+++ MySQL/Task_5/Task_5_Solutions.sql
@@ -37,9 +37,9 @@ select score, dense_rank() over (order by score desc) as 'rank'
     from Scores
     order by score desc;
 
-    -- Department Highest Salary
-    select d.name as Department, e.name as Employee, e.salary as Salary
-    from (select *, dense_rank() over (partition by departmentId order by salary desc) as rnk
-        from Employee) e
-        join Department d on e.departmentId = d.id
-    where e.rnk = 1;
+-- Department Highest Salary
+select d.name as Department, e.name as Employee, e.salary as Salary
+from (select *, dense_rank() over (partition by departmentId order by salary desc) as rnk
+    from Employee) e
+    join Department d on e.departmentId = d.id
+where e.rnk = 1;
